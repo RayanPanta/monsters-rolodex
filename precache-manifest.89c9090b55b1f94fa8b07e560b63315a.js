a58a2f5fff2e8bdf8305116de36c9a138646d4a7
Rayan <rayan.panta@gmail.com>
2020-06-07 21:44:10 +1000

    Updates

---
+++ precache-manifest.89c9090b55b1f94fa8b07e560b63315a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9e529512eba3d004fb4b9867415840",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "95aa482de649551e04d1",
+    "url": "/monsters-rolodex/static/css/main.7da9a7d9.chunk.css"
+  },
+  {
+    "revision": "f7f925811b23f96a37a8",
+    "url": "/monsters-rolodex/static/js/2.caf8c24e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.caf8c24e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95aa482de649551e04d1",
+    "url": "/monsters-rolodex/static/js/main.dcc9131f.chunk.js"
+  },
+  {
+    "revision": "3739ba3ede3c8e2830a7",
+    "url": "/monsters-rolodex/static/js/runtime-main.74944d6b.js"
+  }
+]);
