bb3302e8bd8a8ea4ba0bc1c13456eee03a32e8ef
Rayan <rayan.panta@gmail.com>
2020-06-07 21:44:40 +1000

    deploying the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      gods: [],
+      searchField: ''
+    }
+  }
+  
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ gods:users })) 
+    .catch(error => { console.log(error) })
+  }
+  
+  handleChange = e => {
+   this.setState({ searchField: e.target.value })
+  }
 
-export default App;
+  render(){
+    const { gods, searchField } = this.state;
+    const filteredGods = gods.filter(god => 
+      god.name.toLowerCase().includes(searchField.toLowerCase())  
+    );
+    return(
+      <div className="App">    
+        <h1>God Rolodex</h1> 
+        <SearchBox 
+          placeholder='Search God' 
+          handleChange =  { this.handleChange  }
+        />
+        <CardList gods={filteredGods}>
+        
+        </CardList>        
+      </div>
+      );
+    }
+  }
+  
+  export default App;
+  
